9184c05209c82de3e9f3b67db8dc1abf6a81f458
Lucas Rodrigo Krueger <lucasrkthd@gmail.com>
2021-08-01 15:44:12 -0300

    CRUD and user create option

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,36 @@
 import firebase from './firebaseConnection';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './style.css'
 
 function App() {
+  const [idPost, setIdPost] = useState('')
   const [titulo, setTitulo] = useState('')
   const [autor, setAutor] = useState('')
+  const [posts, setPosts] = useState([])
+
+  const [email, setEmail] = useState('')
+  const [senha , setSenha] = useState('')
   
+
+  useEffect(() =>{
+    async function loadPosts(){
+      await firebase.firestore().collection('posts')
+      .onSnapshot((doc) => {
+        let meusPosts = [];
+        
+        doc.forEach((item) => {
+          meusPosts.push({
+            id: item.id,
+            titulo: item.data().titulo,
+            autor: item.data().autor
+          })
+        })
+        setPosts(meusPosts);
+      })
+    }
+    loadPosts();
+  },[])
+
   async function handleAdd(){
     await firebase.firestore().collection('posts')
     .add({
@@ -24,30 +49,100 @@ function App() {
 
   async function buscaPost(){
     await firebase.firestore().collection('posts')
-    .doc('123')
     .get()
     .then((snapshot) => {
-      setTitulo(snapshot.data().titulo)
-      setAutor(snapshot.data().autor)
+      let lista = [];
+      snapshot.forEach((doc) =>{
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor
+        })
+      })
+      setPosts(lista);
     })
     .catch((error) => {
       console.log(error)
     })
   }
 
+  async function editarPost(){
+    await firebase.firestore().collection('posts')
+    .doc(idPost)
+    .update({
+      titulo: titulo,
+      autor: autor
+    })
+    .then(()=>{
+      alert('Dados atualizados com sucesso')
+      setIdPost('')
+      setTitulo('')
+      setAutor('')
+    })
+    .catch(() => {
+      alert('Erro ao atualizar')
+    })
+  }
+
+  async function excluirPost(id){
+    await firebase.firestore().collection('posts').doc(id)
+    .delete()
+    .then(() => {
+      alert('Excluído com sucesso')
+    })
+  }
+
+  async function novoUsuario(){
+    await firebase.auth().createUserWithEmailAndPassword(email, senha)
+    .then(() => {
+      alert('Usuário cadastrado')
+    })
+    .catch((error)=> {
+      alert(error)
+    })
+  }
+
   return (
-    <div>
+    <div className="app">
       <h1>React + Firebase</h1><br/>
 
       <div className="container">
+        <label>Email</label>
+        <input type="text" value={email} onChange={(e) => {setEmail(e.target.value)}}/><br/>
+        <label>Senha</label>
+        <input type="password" value={senha} onChange={(e) => {setSenha(e.target.value)}}/><br/>
+        <button onClick={novoUsuario}>Cadastrar</button>
+      </div>
+
+      <hr/> <br/>
+
+      <div className="container">
+        <h2>CRUD</h2>
+        <label>ID: </label>
+        <input type="text" value={idPost} onChange={(e) => setIdPost(e.target.value)}/><br/>
+
         <label>Titulo: </label>
-        <textarea type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)}></textarea>
+        <textarea type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)}></textarea><br/>
 
         <label>Autor: </label>
-        <input type="text" value={autor} onChange={(e) => {setAutor(e.target.value)}}/>
+        <input type="text" value={autor} onChange={(e) => {setAutor(e.target.value)}}/><br/>
 
         <button onClick={handleAdd}>Cadastrar</button>
         <button onClick={buscaPost}>Buscar Posts</button>
+        <button onClick={editarPost}>Editar Post</button>
+        
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <span>ID: {post.id}</span><br/>
+                <span>Titulo: {post.titulo}</span><br/>
+                <span>Autor: {post.autor}</span><br/>
+                <button onClick={() => excluirPost(post.id)}>Excluir post</button><br/><br/>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </div>
   );
